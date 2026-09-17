24e47f5b3cc6bfd6f71036a7485b63b903bc1ebf
thomas colligan <thomas.colligan@umontana.edu>
2026-09-17 07:25:22 -0600

    Add -1 to CLASSIC's fch4soil

--- wiemip_registry/CLASSIC/convert.py
+++ wiemip_registry/CLASSIC/convert.py
@@ -118,6 +118,8 @@ class CLASSIC(core.WIEAdapter):
             self.path(experiment, simulation, forcing, factorial, variable),
             decode_times=self.DECODE,
         )
+        if variable == "fch4soil":
+            ds = ds * -1
         da = core.mask_fill(ds[self._get_variable(variable)])
         return core.standardize(da, self.LAT, self.LON, self._time(ds))
 
